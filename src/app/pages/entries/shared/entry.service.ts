1472a21d8ff4419c533dacece70535472b3aa8be
GuilhermeLamim <guilherme.lamim@ambevtech.com.br>
2021-06-30 01:12:57 -0300

    adding graphic chart

--- src/app/pages/entries/shared/entry.service.ts
+++ src/app/pages/entries/shared/entry.service.ts
@@ -3,8 +3,9 @@ import { Observable, throwError } from 'rxjs';
 import { Injectable, Injector } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Entry } from './entry.model';
-import { catchError, mergeMap, switchMap } from 'rxjs/operators';
+import { catchError, map, mergeMap, switchMap } from 'rxjs/operators';
 import { BaseResourceService } from 'src/app/shared/services/base-resource.service';
+import * as moment from 'moment';
 
 
 @Injectable({
@@ -35,5 +36,21 @@ export class EntryService extends BaseResourceService<Entry>{
         }),
         catchError(this.handleError)
       )
+    }
+
+    getByMonthAndYear(month: number, year: number): Observable<Entry[]> {
+      return this.getAll().pipe(
+        map(entries => this.filterByMonthAndYear(entries, month, year))
+      )
+    }
+
+    private filterByMonthAndYear(entries: Entry[], month: number, year: number) {
+      return entries.filter(entry => {
+        const entryDate = moment(entry.date, "DD/MM/YYYY");
+        const monthMatches = entryDate.month() + 1 == month;
+        const yearMatches = entryDate.year() == year;
+  
+        if(monthMatches && yearMatches) return entry;
+      })
     }
 }
---
+++ src/app/pages/reports/reports.module.ts
@@ -0,0 +1,19 @@
+import { SharedModule } from './../../shared/shared.module';
+import { NgModule } from '@angular/core';
+import { ChartModule } from "primeng/chart";
+
+import { ReportsRoutingModule } from './reports-routing.module';
+import { ReportsComponent } from './reports/reports.component';
+
+
+@NgModule({
+  declarations: [
+    ReportsComponent
+  ],
+  imports: [
+    SharedModule,
+    ReportsRoutingModule,
+    ChartModule
+  ]
+})
+export class ReportsModule { }
